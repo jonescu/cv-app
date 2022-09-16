81b45bf5f44d19da6c6e19420a9e9ae00a90718c
Nick Jonescu <nicholasjonescu@gmail.com>
2022-09-16 18:12:56 -0400

    Updates

---
+++ precache-manifest.e6775ad8fce649a3bdb5aa8c3268a8ae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "528db9c4fe3f2e9d42e8",
+    "url": "/cv-app/static/css/main.6f809390.chunk.css"
+  },
+  {
+    "revision": "528db9c4fe3f2e9d42e8",
+    "url": "/cv-app/static/js/main.528db9c4.chunk.js"
+  },
+  {
+    "revision": "d37b95f38b2e340752ae",
+    "url": "/cv-app/static/js/1.d37b95f3.chunk.js"
+  },
+  {
+    "revision": "0fac06044acc7213c87d",
+    "url": "/cv-app/static/js/2.0fac0604.chunk.js"
+  },
+  {
+    "revision": "070135e8f004c9eb9c59",
+    "url": "/cv-app/static/js/3.070135e8.chunk.js"
+  },
+  {
+    "revision": "0e11bd68bf32c466aaf8",
+    "url": "/cv-app/static/js/4.0e11bd68.chunk.js"
+  },
+  {
+    "revision": "0e2a2f405079199f4f3d",
+    "url": "/cv-app/static/js/runtime~main.0e2a2f40.js"
+  },
+  {
+    "revision": "7cff38d9036b427701b93dd3118f0cc7",
+    "url": "/cv-app/index.html"
+  }
+];
